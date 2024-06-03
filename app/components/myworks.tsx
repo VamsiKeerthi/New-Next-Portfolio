62c49e83c1479b220f19d95c0a9e6f834d0f2371
VamsiKeerthi <vamsikeerthi23@gmail.com>
2024-06-03 23:03:49 +0530

    changes done

--- app/components/myworks.tsx
+++ app/components/myworks.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Link from "next/link";
 import { fetchProjects } from "../lib/sanity-utils";
 import Image from "next/image";
-import { GetStaticProps } from "next";
 import { Project } from "../lib/types/Project";
 
 export default async function Projects() {
@@ -10,32 +9,32 @@ export default async function Projects() {
   return (
     <section
       id="projects"
-      className=" grid place-items-center text-sm lg:text-base"
+      className="grid place-items-center text-sm lg:text-base"
     >
       <div className="lg:pt-5 lg:pb-10">
         <h2 className="text-center font-secondary mt-24 font-bold text-3xl text-white">
           My Projects
         </h2>
-        <div className="container mt-5 grid  md:grid-cols-2 lg:grid-cols-2 lg:gap-8 grid-cols-1">
+        <div className="container mt-5 grid md:grid-cols-2 lg:grid-cols-2 lg:gap-8 grid-cols-1">
           {projects.map((project) => (
-            <div className="flex flex-col">
-              <Link
-                href={`/projects/${project.slug}`}
-                className="my-12 rounded-lg hover:scale-105 hover:border-pink-500 transition shadow-lg hover:shadow-pink-700 "
-                key={project._id}
-              >
-                {project.image && (
-                  <Image
-                    src={project.image}
-                    alt={project.name}
-                    width={750}
-                    height={300}
-                    className="object-cover rounded-lg hover:*"
-                  />
-                )}
-                <div className="mt-2 font-extrabold text-white font-secondary">
-                  {project.name}
-                </div>
+            <div className="flex flex-col" key={project._id}>
+              {" "}
+              {/* Added key prop */}
+              <Link href={`/projects/${project.slug}`}>
+                <a className="my-12 rounded-lg hover:scale-105 hover:border-pink-500 transition shadow-lg hover:shadow-pink-700 ">
+                  {project.image && (
+                    <Image
+                      src={project.image}
+                      alt={project.name}
+                      width={750}
+                      height={300}
+                      className="object-cover rounded-lg hover:*"
+                    />
+                  )}
+                  <div className="mt-2 font-extrabold text-white font-secondary">
+                    {project.name}
+                  </div>
+                </a>
               </Link>
             </div>
           ))}
